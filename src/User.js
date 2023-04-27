4a077fa1aa43a8f3e4302b6d512089ff3766d78c
primus02 <drakinspip@gmail.com>
2023-04-27 11:54:36 +0100

    testing..

---
+++ src/User.js
@@ -0,0 +1,60 @@
+import {useState} from "react";
+
+
+
+function User(){
+   const [username, setUsername]= useState("");
+   const [userId, setUserId]= useState(0);
+   const [show, setShow]= useState(false);
+   const [user, setUser]= useState({});
+
+  const handleSubmit=(e)=> {
+    e.preventDefault();
+
+     fetch("http://localhost:3000", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username,
+            userId
+        })
+     })
+     .then(res=> res.json())
+     .then(res=> {
+        if(res.message === "User does not exist"){
+            alert("Invalid username/userId")
+            return;
+        }
+        else{
+                console.log({"profile": res})
+                setUser(res.user);
+                setShow(true);
+        }
+     })
+     .catch(err=> console.log(err));
+
+  }
+
+    return(
+        <div>
+            <h3>Fill the form below to log in</h3>
+            <form onSubmit={(e)=> handleSubmit(e)}>
+                <input required type="text" placeholder="Enter username!" value={username} onChange={(e)=> setUsername(e.target.value)}/>
+                <input required type="number" placeholder="Enter your userId!" value={userId} onChange={(e)=> setUserId(e.target.value)}/>
+                <p><button>Authenticate</button></p>
+            </form>
+
+            {show && (<div>
+                 <h3>User details</h3>
+                 <h4>Name: {user.name}</h4>
+                 <p>Username: {user?.username}</p>
+                 <p>UserId: {user?.userId}</p>
+            </div>)
+            }
+        </div>
+    )
+}
+
+export default User;
